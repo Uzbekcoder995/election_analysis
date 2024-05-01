c7d4c67b2b9246c85e74992ad07b18cb6d4c0ee4
Saidakramov <akbarsaidakramov@gmail.com>
2024-05-01 13:01:06 -0700

    adding files

--- python_practice.py
+++ python_practice.py
@@ -40,3 +40,52 @@ for county in counties_dict.keys():
 
 for voters in counties_dict.values():
     print(voters)
+
+x = 0
+while x <= 5:
+    print(x)
+    x = x + 1
+
+for county in counties:
+    print(county)
+
+for i in range(len(counties)):
+    print(counties[i])
+
+for county in counties_dict:
+    print(counties_dict[county])
+
+for county in counties_dict:
+    print(counties_dict.get(county))
+
+for county, voters in counties_dict.items():
+    print(f' {county} county has {voters} registered voters.')
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+
+for county_dict in voting_data:
+    for value in county_dict.values():
+        print(value)
+
+for county_dict in voting_data:
+    print(county_dict['county'])
+
+for county_dict in voting_data:
+    print(county_dict['registered_voters'])
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+for county, votes in counties_dict.items():
+    print(f'{county} county has {votes:,} registered voters.')
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                 {"county":"Denver", "registered_voters": 463353}, 
+                 {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(f"{county_dict['county']} county has {county_dict['registered_voters']:,} registered voters.")
